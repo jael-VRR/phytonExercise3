43446bc631f6e0537e22fdea4cd536f0807c518d
jael-VRR <jrodriguezruidias@gmail.com>
2021-08-25 21:23:28 -0500

    exercise ecuacion

---
+++ src/exercise.py
@@ -0,0 +1,5 @@
+#Creaamos la variable que contiene el dato a ingresar
+x =float(input("Ingrese un valor entero para x : "))
+#variable que contiene la operación 
+y =1/(x+1/(x+1/(x+1/x)))
+print("y=",y)
