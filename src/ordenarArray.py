ad84b1a3a67fde26f586793858f7820129d6f75a
jael-VRR <jrodriguezruidias@gmail.com>
2021-08-25 21:27:55 -0500

    exercise sortArray

---
+++ src/ordenarArray.py
@@ -0,0 +1,9 @@
+#Ejercicio n°4
+#Inicializar la variable que contiene el array a ordenar
+array= [1, 6, 2, 3, 7, 7, 8, 9]
+print(array)
+numeros = [1, 6, 2, 3, 7, 7, 8, 9]
+#utilizar la función sort que ordena los elementos del array
+numeros.sort()
+#muestra en consola el resultado
+print(numeros)
